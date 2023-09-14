451a7698f90f4793bd76c4dabc06539903e61985
greennarae <smk5647@gmail.com>
2023-09-14 14:28:39 +0900

    deploy: kustomization.yaml

--- startFront.sh
+++ startFront.sh
@@ -1,4 +1,4 @@
-rm -rf /var/lib/apt/lists/* && \
-rm /etc/nginx/sites-enabled/default && \
-service nginx start
-(cd /goormService/frontend && npm i && npm start)
+#rm -rf /var/lib/apt/lists/* && \
+#rm /etc/nginx/sites-enabled/default && \
+#service nginx start
+#(cd /goormService/frontend && npm i && npm start)
